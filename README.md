2a7869ceb9264c63a84a70917006c2a2c11a1daa
Mingwei Zhang <mingwei@mwzhang.com>
2023-05-26 10:58:30 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # asnames-rs
-Autonomous System (AS) names and country lookup Rust library
+
+Simple Autonomous System (AS) names and country lookup Rust library
+
+## Data source
+
+- <https://ftp.ripe.net/ripe/asnames/asn.txt>
+
+## Data definition
+
+```rust
+#[derive(Debug, Clone)]
+pub struct AsName {
+    pub asn: u32,
+    pub name: String,
+    pub country: String,
+}
+```
+
+## Usage
+
+```rust
+use std::collections::HashMap;
+use asnames::{AsName, load_asnames};
+
+let asnames: HashMap<u32, AsName> = load_asnames().unwrap();
+assert_eq!(asnames.get(&3333).unwrap().name, "RIPE-NCC-AS Reseaux IP Europeens Network Coordination Centre (RIPE NCC)");
+assert_eq!(asnames.get(&400644).unwrap().name, "BGPKIT-LLC");
+assert_eq!(asnames.get(&400644).unwrap().country, "US");
+```
---
+++ src/lib.rs
@@ -0,0 +1,69 @@
+//! asnames-rs is a library for simple Autonomous System (AS) names and country lookup
+//!
+//! # Data source
+//!
+//! - <https://ftp.ripe.net/ripe/asnames/asn.txt>
+//!
+//! # Data structure
+//!
+//! ```rust
+//! #[derive(Debug, Clone)]
+//! pub struct AsName {
+//!     pub asn: u32,
+//!     pub name: String,
+//!     pub country: String,
+//! }
+//! ```
+//!
+//! # Example
+//!
+//! ```rust
+//! use std::collections::HashMap;
+//! use asnames::{AsName, load_asnames};
+//!
+//! let asnames: HashMap<u32, AsName> = load_asnames().unwrap();
+//! assert_eq!(asnames.get(&3333).unwrap().name, "RIPE-NCC-AS Reseaux IP Europeens Network Coordination Centre (RIPE NCC)");
+//! assert_eq!(asnames.get(&400644).unwrap().name, "BGPKIT-LLC");
+//! assert_eq!(asnames.get(&400644).unwrap().country, "US");
+//! ```
+
+use anyhow::Result;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct AsName {
+    pub asn: u32,
+    pub name: String,
+    pub country: String,
+}
+
+const DATA_URL: &str = "https://ftp.ripe.net/ripe/asnames/asn.txt";
+
+pub fn load_asnames() -> Result<HashMap<u32, AsName>> {
+    let lines = oneio::read_lines(DATA_URL)?;
+    let asnames = lines
+        .into_iter()
+        .filter_map(|line| {
+            let text = line.ok().unwrap();
+            let (asn_str, name_country_str) = match text.split_once(" ") {
+                Some((asn, name)) => (asn, name),
+                None => return None,
+            };
+            let (name_str, country_str) = match name_country_str.rsplit_once(", ") {
+                Some((name, country)) => (name, country),
+                None => return None,
+            };
+            Some(AsName {
+                asn: asn_str.parse::<u32>().unwrap(),
+                name: name_str.to_string(),
+                country: country_str.to_string(),
+            })
+        })
+        .collect::<Vec<AsName>>();
+
+    let mut asnames_map = HashMap::new();
+    for asname in asnames {
+        asnames_map.insert(asname.asn, asname);
+    }
+    Ok(asnames_map)
+}
